e26de5ab4caf04c35c1bfe2dfb863a69930bc573
tthibodeaux <TThibodeaux@ap.org>
2016-03-09 19:27:32 -0600

    Part one edits complete

---
+++ NICAR2016/part3/create_contributions.sql
@@ -0,0 +1,15 @@
+CREATE TABLE "contributions" (
+"id" INTEGER PRIMARY KEY  AUTOINCREMENT  NOT NULL  UNIQUE , 
+"committee_id" VARCHAR, 
+"entity_type" VARCHAR, 
+"last_name" VARCHAR, 
+"first_name" VARCHAR, 
+"middle_name" VARCHAR, 
+"city" VARCHAR, 
+"state" VARCHAR, 
+"zip_code" VARCHAR, 
+"employer" VARCHAR, 
+"occupation" VARCHAR, 
+"date" DATETIME, 
+"amount" INTEGER
+);
